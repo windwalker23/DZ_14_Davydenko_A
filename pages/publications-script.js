160535a40bbbd3d205ec6d5fe4c15c190d4b379c
windwalker23 <yeskennyt@gmail.com>
2022-05-04 14:08:17 +0300

    v1.1

---
+++ pages/publications-script.js
@@ -0,0 +1,50 @@
+const showModalButton = document.getElementById("contact")
+const modalElement = document.getElementById("modal")
+const modalCloseButton = document.getElementById("modal-close")
+
+showModalButton.addEventListener("click", function () {
+    modalElement.classList.add("active-modal")
+})
+
+modalCloseButton.addEventListener("click", function () {
+    modalElement.classList.remove("active-modal")
+})
+
+const showNavButton = document.getElementById("nav__button-mob")
+const navElement = document.getElementById("nav-mob")
+const body = document.getElementById("body")
+
+showNavButton.addEventListener("click", function () {
+    showNavButton.classList.add("if-nav-mob-open")
+    navElement.classList.add("nav-mob-container-open")
+    body.classList.add("no-overflow")
+})
+
+showNavButton.addEventListener("dblclick", function () {
+    showNavButton.classList.remove("if-nav-mob-open")
+    navElement.classList.remove("nav-mob-container-open")
+    body.classList.remove("no-overflow")
+})
+
+const showModalButtonMob = document.getElementById("contact-mob")
+
+showModalButtonMob.addEventListener("click", function () {
+    modalElement.classList.add("active-modal")
+})
+
+modalCloseButton.addEventListener("click", function () {
+    modalElement.classList.remove("active-modal")
+})
+
+const newsButton = document.getElementById("button-news")
+const newsArticles = document.getElementById("button-articles")
+
+newsButton.addEventListener("click", function () {
+    newsButton.classList.add("segment-active")
+    newsArticles.classList.remove("segment-active")
+})
+
+newsArticles.addEventListener("click", function () {
+    newsArticles.classList.add("segment-active")
+    newsButton.classList.remove("segment-active")
+})
